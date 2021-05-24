85d53f4b3acdab6ac40547492080e9883531080b
Ivashchenko Ivan <62796151+IvashchenkoIvanIgorevich@users.noreply.github.com>
2021-05-24 10:34:16 +0300

    Add files via upload

---
+++ PetrolStation/PetrolStation/Model/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace PetrolStation.Model
+{
+    public class Category
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<Category> SubCategories { get; set; } = new List<Category>();
+        public List<Product> Products { get; set; } = new List<Product>();
+
+        public Category(IEnumerable<Category> subCategories)
+        {
+            SubCategories = subCategories;
+        }
+    }
+}
